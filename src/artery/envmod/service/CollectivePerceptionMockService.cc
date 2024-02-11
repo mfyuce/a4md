3acca2a297875adb508b78b02e4d162184a0d22d
fatih.yuce <fatih.yuce@ulakhaberlesme.com.tr>
2024-02-11 03:44:43 +0300

    initial commit to c1

--- src/artery/envmod/service/CollectivePerceptionMockService.cc
+++ src/artery/envmod/service/CollectivePerceptionMockService.cc
@@ -225,6 +225,83 @@ void CollectivePerceptionMockService::recordPacket(CollectivePerceptionMockMessa
     ostream_mutex.unlock();
 }
 
+
+void CollectivePerceptionMockService::recordPacket(CollectivePerceptionMockMessage& cpm, std::stringstream& buffer)
+{
+    ostream_mutex.lock();
+    using namespace vanetza;
+    buffer << "{";
+    {
+        buffer << "\"step\":" << omnetpp::simTime().dbl() << ","
+               << "\"destination_port\":" << host_cast<PortNumber>(getPortNumber()) << ","
+               << "\"transport_type\":" << static_cast<int>(geonet::TransportType::SHB) << ","
+           << "\"tc_id\":" << mDccProfile << ","
+           << "\"communication_profile\":" << static_cast<int>(geonet::CommunicationProfile::ITS_G5) << ",";
+
+
+    buffer << "\"fov_containers\": [ ";
+        {
+    for (CollectivePerceptionMockMessage::FovContainer fovContainer : cpm.getFovContainers()) {
+        buffer << "{"
+               << "\"angle\":" << fovContainer.fov.angle.value() << ","
+               << "\"range\":" << fovContainer.fov.range.value() << ","
+               << "\"position\":" << static_cast<int>(fovContainer.position) << ","
+               << "\"sensorId\":" << fovContainer.sensorId << "},";
+    }
+        };
+    buffer << "],";
+
+    buffer << "\"last_fov\":" << mFovLast << ",";
+
+    buffer << "\"tracked_objects\": [ ";
+        {
+    for (const auto& objectContainer : cpm.getObjectContainers()) {
+            buffer << "{";
+            {
+        buffer << "\"objectId\":" << objectContainer.objectId << ","
+               << "\"sensorId\":" << objectContainer.sensorId << ","
+               << "\"timeOfMeasurement\":" << objectContainer.timeOfMeasurement << ",";
+
+        EnvironmentModelObject* o = objectContainer.object.lock().get();
+        if (o != NULL) {
+            buffer << "\"object\": { "
+                   << "\"center_x\":" << o->getCentrePoint().x.value() << ","
+                   << "\"center_y\":" << o->getCentrePoint().y.value() << ","
+                   << "\"external_id\":\"" << o->getExternalId() << "\","
+                   << "\"length\":" << o->getLength().value()
+                   << ","
+                   // TODO: o->getOutline()
+                   << "\"radius\":" << o->getRadius().value() << ","
+                   << "\"width\":" << o->getWidth().value() << ","
+                   << "\"vehicle\": {"
+                   << "\"x\":" << o->getVehicleData().position().x.value() << ","
+                   << "\"y\":" << o->getVehicleData().position().y.value() << ","
+                   << "\"acceleration\":" << o->getVehicleData().acceleration().value() << ","
+                   << "\"curvature\":" << o->getVehicleData().curvature().value() << ","
+                   << "\"curvature_confidence\":" << o->getVehicleData().curvature_confidence() << ","
+                   << "\"station_id\":" << o->getVehicleData().station_id() << ","
+                   << "\"station_type\":" << static_cast<int>(o->getVehicleData().getStationType()) << ","
+                   << "\"heading\":" << o->getVehicleData().heading().value() << ","
+                   << "\"latitude\":" << o->getVehicleData().latitude().value() << ","
+                   << "\"longitude\":" << o->getVehicleData().longitude().value() << ","
+                   << "\"speed\":" << o->getVehicleData().speed().value() << ","
+                   << "\"updated\":" << o->getVehicleData().updated().dbl() << ","
+                           << "\"yaw_rate\":" << o->getVehicleData().yaw_rate().value() << "}"
+                   << "}";
+        }
+        buffer << "},";
+    }
+        }
+    buffer << "],";
+        };
+
+    buffer << "\"length\":" << cpm.getByteLength() << ","
+                  << "\"mHostId\":" << mHostId << "},"
+                  << "\n";
+    };
+
+    ostream_mutex.unlock();
+}
 void CollectivePerceptionMockService::generatePacket()
 {
     using namespace vanetza;
@@ -276,8 +353,32 @@ CollectivePerceptionMockService::CollectivePerceptionMockService():mGenerateAfte
 //    generatedCPMs << "[";
 //    receivedCPMs << "[";
 }
+CollectivePerceptionMockService::CollectivePerceptionMockService():mGenerateAfterCam(false)
+{
+//    generatedCPMs << "[";
+//    receivedCPMs << "[";
+}
+
 
 } // namespace artery
+//sed  -E  's/\},]/}]/g' generate.txt > generate_edited.json && sed  -i -E 's/,\}/}/g'  generate_edited.json
+
+//sed  -E  's/\},]/}]/g' generate.txt > generate_edited.json && sed  -i -E 's/,\}/}/g'  generate_edited.json
+//sed  -E  's/\},]/}]/g' receive.txt > receive_edited.json && sed  -i -E 's/,\}/}/g'  receive_edited.json
+//sed  -E  's/\},]/}]/g' indicate.txt > indicate_edited.json && sed  -i -E 's/,\}/}/g'  indicate_edited.json
+//sed  -E  's/\},]/}]/g' env.txt > env_edited.json && sed  -i -E 's/,\}/}/g'  env_edited.json
+//attacks: https://github.com/quic/vasp
+//https://github.com/quic/autopen?tab=readme-ov-file
+//https://www.ncbi.nlm.nih.gov/pmc/articles/PMC9920388/
+//https://www.youtube.com/watch?v=xfFjaMn9z8s&themeRefresh=1
+//https://github.com/ibr-cm/a4md
+//https://ieeexplore.ieee.org/document/10136315/authors#authors
+//https://www.diva-portal.org/smash/get/diva2:1440195/FULLTEXT01.pdf
+//https://github.com/topics/v2x?l=c%2B%2B
+//https://groups.google.com/g/omnetpp/c/F5NJKLcOaXI?pli=1
+//https://webthesis.biblio.polito.it/29324/1/tesi.pdf
+//https://dl.acm.org/doi/abs/10.1145/3565287.3617616
+//https://www.google.com/search?q=A4MD&sourceid=chrome&ie=UTF-8#ip=1
 //sed  -E  's/\},]/}]/g' generate.txt > generate_edited.json && sed  -i -E 's/,\}/}/g'  generate_edited.json
 
 //sed  -E  's/\},]/}]/g' generate.txt > generate_edited.json && sed  -i -E 's/,\}/}/g'  generate_edited.json
